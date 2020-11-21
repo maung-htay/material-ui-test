0cfebce138dbd5b996f50273414b2bbdf0a15bf0
maung-htay <ukyishwejp@gmail.com>
2020-11-21 22:23:28 +0630

    Test Git

--- src/material_test/Test.jsx
+++ src/material_test/Test.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { Typography, makeStyle, AppBar, Toolbar, Button, IconButton } from "@material-ui/core";
-import { Menu as MenuIcon } from '@material-ui/icons'
+import {
+  Typography,
+  makeStyle,
+  AppBar,
+  Toolbar,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+import { Menu as MenuIcon } from "@material-ui/icons";
 
 const useStyle = makeStyles({});
 
 const Test = () => {
   return (
-      <AppBar position="fixed" color="secondary">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">
-             
-          </Typography>
-          muiBo
-        </Toolbar>
-      </AppBar>
-      
-  )
-  
-  
+    <AppBar position="fixed" color="secondary">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6"></Typography>
+        muiBo hlahla
+      </Toolbar>
+    </AppBar>
+  );
 };
 export default Test;
